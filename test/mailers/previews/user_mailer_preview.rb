52bfddef2452c7214c78700242678bb34189df34
Takuma <takumab@gmail.com>
2016-02-10 11:43:21 +0000

    Add rspec and delete test

--- test/mailers/previews/user_mailer_preview.rb
+++
@@ -1,6 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/user_mailer
-class UserMailerPreview < ActionMailer::Preview
-	def contact_form
-		UserMailer.contact_form("takumab@gmail.com", "Takuma", "Hello Man!")
-	end
-end
