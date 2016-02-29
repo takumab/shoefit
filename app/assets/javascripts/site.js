21a63e52f5e5bd1158059aff9bc3a4a81acc1e99
Takuma <takumab@gmail.com>
2016-02-29 00:45:13 +0000

    Added jQuery plugin elevateZoom

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -5,7 +5,27 @@ $(document).on('ready page:load', function(){
 		score: function() {
 			return $(this).attr('data-score');
 		}
-	}); //Raty rating stars
-
+	}); // Raty rating stars
 	
+
+	$('.img-zoom').elevateZoom({
+			
+			scrollZoom 		: true,
+			borderSize		: 2,
+			borderColour 		: "rgb(136,136,136)",
+			tintColour 		: "#e7e7e7",
+			zoomWindowWidth 	: 200,
+			zoomWindowHeight 	: 150,
+			zoomWindowPosition	: 1,
+			zoomWindowOffetx	: 12,
+			zoomWindowOffety	: 0,
+			//zoomWindowFadeIn	: 500,
+			zoomWindowFadeOut	: 500,
+			lensFadeIn		: 500,
+			lensFadeOut		: 500 
+
+			
+			
+			
+	}); // elevateZoom on show page
 });
